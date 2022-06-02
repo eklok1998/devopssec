ec74553b57ab6ac1fee09f0e6633d4db39edeeab
eklok1998 <eekaylok@gmail.com>
2022-06-02 22:52:04 +0100

    Committing the script to GitHub

---
+++ devops-labs-2-task.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd /Users/eekaylok/Documents/NCI/Advanced-Client-Side/Week2
+
+mkdir fol_1
+mkdir fol_2
+
+touch fol_1/1_1.txt
+touch fol_1/1_2.txt
+touch fol_1/1_3.txt
+
+touch fol_2/1_1.txt
+touch fol_2/1_2.txt
+touch fol_2/1_3.txt
+
+touch .gitignore.txt
+sudo nano /Users/eekaylok/Documents/NCI/Advanced-Client-Side/Week2/.gitignore.txt
+#I entered the path names to fol_1 and fol_2 in the .gitignore file.
+
+
+
+
